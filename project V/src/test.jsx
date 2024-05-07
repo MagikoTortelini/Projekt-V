df690d5734c582e86b5fdac12597142765a08d12
MagikoTortelini <k.pawlakq@gmail.com>
2024-05-07 16:34:36 +0200

    tak są zmiany tf

--- project V/src/test.jsx
+++ project V/src/test.jsx
@@ -2,16 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function Test() {
+  const [film_id, setFilm_id] = useState("https://ebd.cda.pl/620x368/1648962228");
+  const changeImageSrc = (e) => {
+    setImageSrc('https://ebd.cda.pl/620x368/'+ e);
+  }
   return (
     <>
-      <div id='t'>
-        <iframe  src="https://streamable.com/e/4vdkay" width="100%" height="100%" ></iframe>
-       
+      <div id='player'>
+        <iframe src="https://www.dailymotion.com/video/1ht9n4c3r3fnst9ddl1" width="100%" height="100%" scrolling="no" allowFullScreen={true} name="v2" allow="encrypted-media"></iframe>
+        <iframe frameBorder="0" width="640" height="360" 
+                src="https://geo.dailymotion.com/player/1ht9n4c3r3fnst9ddl1.html?video=x84sh87" 
+                allowFullScreen={true}
+                allow="autoplay; fullscreen; picture-in-picture; web-share">
+</iframe>
       </div>
       <div id='z'>
-        <iframe  src="https://player.twitch.tv/?channel=veniakk&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no" height="600" width="700"></iframe>
+        <iframe  src="https://player.twitch.tv/?channel=veniakk&parent=localhost" frameorder="0" allowFullScreen={true} scrolling="no" height="30%" width="100%"></iframe>
+        <iframe src="https://www.twitch.tv/embed/veniakk/chat?darkpopout&parent=localhost"
+        height="70%"
+        width="100%">
+      </iframe>
       </div>
-      
     </>
   )
 }
